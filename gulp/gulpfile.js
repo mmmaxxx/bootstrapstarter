20a5639502dac90ac14fbe1353424b724f1ad352
mmmaxxx <max.webhk@gmail.com>
2019-04-12 09:23:34 +0800

    Exclude node modules

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -1,57 +1,88 @@
-var gulp = require("gulp"),
-    sass = require("gulp-sass"),
-    postcss = require("gulp-postcss"),
-    autoprefixer = require("autoprefixer"),
-    cssnano = require("cssnano"),
-    sourcemaps = require("gulp-sourcemaps"),
-    browserSync = require("browser-sync").create(),
-    plumber = require("gulp-plumber"),
-    notify = require("gulp-notify"),
-    concat = require("gulp-concat"),
-    rename = require("gulp-rename");
+// ----- CHANGE THIS! ----- //
+var localUrl = 'local.oscarmaxime.com';
 
-var paths = {
-    styles: {
-        scss: "../assets/scss/**/*.scss",
-        css: "../assets/css"
-    },
-    html: "../*.html"
-};
 
-function scss() {
-    return gulp
-        .src(paths.styles.scss)
+// ----- run 'gulp' ----- //
+var gulp = require('gulp'),
+    promise = require('es6-promise').polyfill(),
+    sass = require('gulp-sass'), // quicker than ruby-sass!
+    concat = require('gulp-concat'),
+    imagemin = require('gulp-imagemin'),
+    plumber = require('gulp-plumber'),
+    newer = require('gulp-newer'),
+    browserSync = require('browser-sync').create(),
+    sourcemaps = require('gulp-sourcemaps'),
+    autoprefixer = require('gulp-autoprefixer'),
+    jshint = require('gulp-jshint'),
+    rename = require('gulp-rename');
 
+
+
+// ----- Build SASS files ----- //
+gulp.task('sass', function () {
+
+    return gulp.src(['../assets/scss/*.scss', '../assets/scss/**/*.scss'])
         .pipe(sourcemaps.init())
-        .pipe(plumber({
-            errorHandler: notify.onError("Error: <%= error.message %>")}))
-        .pipe(sass({
-            outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(postcss([autoprefixer(),cssnano({
-            discardComments: {removeAll: true}})]))
-
-        .pipe(concat("main.css"))
-        .pipe(rename({ suffix: ".min" }))
-
+        .pipe(sass())
+        .on('error', swallowError)
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest(paths.styles.css))
-
+        .pipe(autoprefixer())
+        .pipe(concat('main.min.css'))
+        .pipe(gulp.dest('../assets/css'))
         .pipe(browserSync.stream());
-}
+});
 
-function reload() {
-    browserSync.reload();
-}
 
-function watch() {
+// ----- Build JS files ----- //
+gulp.task('js', function () {
+    return gulp.src('../assets/js/src/*.js')
+        .pipe(newer('../assets/js/build'))
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish'))
+        .pipe(plumber({
+            errorHandler: function (err) {
+                console.log(err);
+                this.emit('end');
+            }
+        }))
+
+        .pipe(concat('script.js'))
+        .pipe(gulp.dest('../assets/js/build'));
+
+});
+
+
+
+// ----- Watch function ----- //
+gulp.task('default', function () {
+
     browserSync.init({
-        server: "..",
-        notify: false
+        proxy: localUrl,
+        injectChanges: true,
+        socket: {
+            domain: 'localhost:3000'
+        }
     });
-    gulp.watch(paths.styles.scss, scss)
-    gulp.watch(global + "/html.css")
+
+
+    gulp.watch('../assets/scss/*.scss', ['sass']);
+    gulp.watch('../assets/scss/**/*.scss', ['sass']);
+    gulp.watch('../assets/js/src/*.js', ['js']);
+    gulp.watch('../assets/js/build/*.js', ['reload']);
+
+
+});
+
+
+// ----- Reload after Javascript Change ----- //
+gulp.task('reload', [], function () {
+    browserSync.reload();
+});
+
+function swallowError(error) {
+
+    // If you want details of the error in the console
+    console.log(error.toString());
+
+    this.emit('end');
 }
-
-gulp.task('default', gulp.parallel(scss, watch));
-
-exports.watch = watch;
